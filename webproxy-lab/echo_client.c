5ebdd8f35b556f9f8c5fc71a8a1bdb6d03488222
RedHong56 <redhong9956@gmail.com>
2025-10-31 19:20:08 +0900

    Echo_server_client

--- webproxy-lab/echo_client.c
+++ webproxy-lab/echo_client.c
@@ -6,24 +6,20 @@ int main(int argc, char **argv)
     char *host, *port, buf[MAXLINE];
     rio_t rio;
 
-    if(argc != 3) // 인자가 <호스트 주소> , <포트 번호> 를 넘으면 안되기 때문에 예외 처리?
-    {
-        fprintf(stderr, "usage: %s<host> <port>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
         exit(0);
     }
-
     host = argv[1];
     port = argv[2];
 
-    clientfd = Open_clientfd(host,port);
+    clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL)
-    {
-        Rio_writen(clientfd, buf, strlen(MAXLINE));
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf));
         Rio_readlineb(&rio, buf, MAXLINE);
         Fputs(buf, stdout);
     }
-    Close(clientfd);
-    exit(0);
+    Close(clientfd); 
 }
